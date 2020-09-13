6c0649f3e8e4700bae4dca42fa2da4e72147ddab
leef <leevs2017@hotmail.com>
2020-09-13 17:30:23 +0800

    chapter1.1

---
+++ chapter1.1/src/q1_13.java
@@ -0,0 +1,14 @@
+
+import edu.princeton.cs.algs4.StdOut;
+public class q1_13 {
+    public static  void main(String[] args){
+        int a=Integer.parseInt(args[0]);
+        int b=Integer.parseInt(args[1]);
+        int c=Integer.parseInt(args[2]);
+        if (a==b && b==c ){
+            StdOut.println("equal");
+        }else{
+            StdOut.println("not equal");
+        }
+    }
+}
